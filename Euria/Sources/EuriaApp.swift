10fe39e83330f4f5c90c0829a7fac33063f50a7e
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-10-21 14:06:57 +0200

    feat: Add Sentry

--- Euria/Sources/EuriaApp.swift
+++ Euria/Sources/EuriaApp.swift
@@ -23,6 +23,8 @@ import SwiftUI
 
 @main
 struct EuriaApp: App {
+    // periphery:ignore - Making sure the Sentry is initialized at a very early stage of the app launch.
+    private let sentryService = SentryService()
     // periphery:ignore - Making sure the DI is registered at a very early stage of the app launch.
     private let dependencyInjectionHook = TargetAssembly()
 
