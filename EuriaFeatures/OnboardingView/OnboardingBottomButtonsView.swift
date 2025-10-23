3aed69c958e2faf4db4b2acfee8f9ae9486538db
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-10-23 15:21:24 +0200

    fix: Remove unused var

--- EuriaFeatures/OnboardingView/OnboardingBottomButtonsView.swift
+++ EuriaFeatures/OnboardingView/OnboardingBottomButtonsView.swift
@@ -19,7 +19,6 @@
 import AuthenticationServices
 import DesignSystem
 import EuriaCore
-import EuriaCoreUI
 import EuriaResources
 import InfomaniakCoreUIResources
 import InfomaniakDI
@@ -31,7 +30,6 @@ struct OnboardingBottomButtonsView: View {
     @ObservedObject var loginHandler = LoginHandler()
 
     @State private var excludedUserIds: [AccountManagerable.UserId] = []
-    @State private var isPresentingCreateAccount = false
 
     @Binding var selection: Int
     let slideCount: Int
