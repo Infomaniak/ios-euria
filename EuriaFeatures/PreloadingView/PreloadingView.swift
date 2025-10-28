9b4ba28e8f9cca45dca97b70465d0add5c27114c
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-10-28 18:41:39 +0100

    feat: Add LaunchScreen

--- EuriaFeatures/PreloadingView/PreloadingView.swift
+++ EuriaFeatures/PreloadingView/PreloadingView.swift
@@ -25,16 +25,6 @@ import InfomaniakCoreSwiftUI
 import InfomaniakDI
 import SwiftUI
 
-extension VerticalAlignment {
-    enum SplashScreenIconAlignment: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            return context[VerticalAlignment.center]
-        }
-    }
-
-    static let splashScreenIconAlignment = VerticalAlignment(SplashScreenIconAlignment.self)
-}
-
 public struct PreloadingView: View {
     @LazyInjectService private var accountManager: AccountManagerable
 
@@ -47,7 +37,7 @@ public struct PreloadingView: View {
     public init() {}
 
     public var body: some View {
-        ZStack(alignment: Alignment(horizontal: .center, vertical: .splashScreenIconAlignment)) {
+        ZStack(alignment: .center) {
             backgroundImage
                 .resizable()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -58,7 +48,6 @@ public struct PreloadingView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(maxWidth: 200)
-                    .alignmentGuide(.splashScreenIconAlignment) { d in d[VerticalAlignment.center] }
 
                 ProgressView()
                     .progressViewStyle(.circular)
