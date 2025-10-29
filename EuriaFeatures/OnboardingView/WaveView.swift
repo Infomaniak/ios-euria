2137c45a87aa59926008d5be2cdfffc9dad9cb27
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-10-29 11:23:39 +0100

    fix: Remove header logo for now

--- EuriaFeatures/OnboardingView/WaveView.swift
+++ EuriaFeatures/OnboardingView/WaveView.swift
@@ -48,7 +48,7 @@ struct WaveView<BottomView: View>: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Context) -> OnboardingViewController {
         let configuration = OnboardingConfiguration(
-            headerImage: headerImage,
+            headerImage: nil,
             slides: slides,
             pageIndicatorColor: UIColor.tintColor,
             isScrollEnabled: true,
