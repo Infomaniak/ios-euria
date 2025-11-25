234b155611ae33566af327b22c7857ea48d98df7
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-11-25 09:17:15 +0100

    feat: Handle locally
    # Conflicts:
    #	EuriaFeatures/MainView/Delegate/EuriaWebViewDelegate+WKNavigationDegate.swift
    #	EuriaFeatures/MainView/MainView.swift

--- EuriaFeatures/MainView/MainView.swift
+++ EuriaFeatures/MainView/MainView.swift
@@ -59,7 +59,7 @@ public struct MainView: View {
     public init(session: any UserSessionable) {
         self.session = session
         _webViewDelegate = StateObject(wrappedValue: EuriaWebViewDelegate(
-            host: ApiEnvironment.current.euriaHost,
+            host: "local.euria.preprod.dev.infomaniak.ch",
             session: session
         ))
     }
@@ -68,7 +68,7 @@ public struct MainView: View {
         ZStack {
             if isShowingWebView {
                 WebView(
-                    url: URL(string: "https://\(ApiEnvironment.current.euriaHost)/")!,
+                    url: URL(string: "https://local.euria.preprod.dev.infomaniak.ch:5173/")!,
                     webConfiguration: webViewDelegate.webConfiguration,
                     webViewCoordinator: webViewDelegate
                 )
