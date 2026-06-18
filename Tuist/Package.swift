625d25663cddcb9f131aba38ffb90d25f25caac4
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-18 13:46:11 +0200

    chore: Bump package version and add UIKit package

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -13,6 +13,7 @@ let packageSettings = PackageSettings(
         "InfomaniakConcurrency": .framework,
         "InfomaniakCoreCommonUI": .framework,
         "InfomaniakCoreSwiftUI": .framework,
+        "InfomaniakCoreUIKit": .framework,
         "InfomaniakCoreUIResources": .framework,
         "InfomaniakNotifications": .framework,
         "InfomaniakCore": .framework,
@@ -33,15 +34,16 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "9.8.0")),
         .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "18.9.0")),
-        .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "24.2.0")),
-        .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "23.2.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "25.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core-uikit", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "24.0.1")),
         .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.5")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.6.0")),
+        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "9.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.5.0")),
         .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "15.1.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.4.3")),
-        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "16.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "17.0.0")),
         .package(url: "https://github.com/Infomaniak/swift-concurrency", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/matomo-org/matomo-sdk-ios", .upToNextMajor(from: "7.7.0"))
     ]
