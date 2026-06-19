2364404cf85c831defa742358b0abada1563d5aa
Killian Mathias <killian.mathias@infomaniak.com>
2026-06-19 09:56:17 +0200

    feat: Insert new font scale accessibility

---
+++ EuriaCoreUI/Utils/FontHelper.swift
@@ -0,0 +1,47 @@
+/*
+ Infomaniak Euria - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import UIKit
+
+public enum FontHelper {
+    public static func loadCSS() -> String {
+        let scaleFactor = fontScale()
+        let css = """
+        :root{
+        --app-font-scale: \(scaleFactor);
+        }
+        """
+
+        let script = """
+        var style = document.createElement('style');
+        style.textContent = `\(css)`;
+        document.head.appendChild(style);
+        """
+
+        return script
+    }
+
+    private static func fontScale() -> CGFloat {
+        let standardSize: CGFloat = 16.0
+        let fontMetrics = UIFontMetrics(forTextStyle: .body)
+        let scaledSize = fontMetrics.scaledValue(for: standardSize)
+        let scaleFactor = scaledSize / standardSize
+        return scaleFactor
+    }
+}
